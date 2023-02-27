d18566178fbd1953e2562abec8b669a4bcc92812
LuIsMoReNo7 <1483853@senati.pe>
2023-02-27 11:01:20 -0500

    usamos import tkinter as tk para poder usar el tkinter

---
+++ proyecto.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class Formulario(tk.Frame):
+    def __init__(self,vp=None):
+        #Heredar las caracteristicas de la clase base de tkinter
+        
