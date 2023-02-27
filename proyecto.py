4b54c2b34750445f602d71d90fc949b16ccfe8f2
LuIsMoReNo7 <1483853@senati.pe>
2023-02-27 11:55:47 -0500

    realizamos lo mismo pero esta vez cambiando el texto de etiqueta por el que nos pide

--- proyecto.py
+++ proyecto.py
@@ -8,11 +8,23 @@ class Formulario(tk.Frame):
         vp.title("SISTEMA DE REGISTRO DE PEDIDOS")
         vp.geometry("600x600")
         
-        self.lbl0=tk.Label(vp,text="FERRETERIA EL TORINILLO FELIZ:")#Etiqueta
-        self.lbl0.grid(column=2,row=1,padx=8,pady=11)
+        self.lbl0=tk.Label(vp,text="FERRETERIA EL TORINILLO FELIZ")#Etiqueta
+        self.lbl0.grid(column=3,row=1,padx=8,pady=11)
         
         self.lbl1=tk.Label(vp,text="DNI:")#Etiqueta
         self.lbl1.grid(column=0,row=2,padx=8,pady=11)
         
         self.txtn1=tk.Entry(vp,width=20)
         self.txtn1.grid(column=2,row=2)
+        
+        self.lbl2=tk.Label(vp,text="Nombres:")#Etiqueta
+        self.lbl2.grid(column=3,row=3,padx=8,pady=11)
+        
+        self.txtn2=tk.Entry(vp,width=20)
+        self.txtn2.grid(column=4,row=3)
+        
+        self.lbl3=tk.Label(vp,text="apellidos:")#Etiqueta
+        self.lbl3.grid(column=0,row=3,padx=8,pady=11)
+        
+        self.txtn3=tk.Entry(vp,width=20)
+        self.txtn3.grid(column=2,row=3)
