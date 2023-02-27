5cc5669b8c826f8bf558f621e1c10426eb21dd73
LuIsMoReNo7 <1483853@senati.pe>
2023-02-27 11:06:44 -0500

    le ponemos un titulo y tamaño a la grafica usando vp.

--- proyecto.py
+++ proyecto.py
@@ -4,4 +4,7 @@ from tkinter import messagebox
 class Formulario(tk.Frame):
     def __init__(self,vp=None):
         #Heredar las caracteristicas de la clase base de tkinter
+        super().__init__(vp)
+        vp.title("SISTEMA DE REGISTRO DE PEDIDOS")
+        vp.geometry("600x600")
         
